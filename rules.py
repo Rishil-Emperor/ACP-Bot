c1d1df68eeade5e1b2444cd8c0393460118429e2
Rishil Kadakia <rishil.kadakia@gmail.com>
2020-12-26 03:35:49 -0700

    Token As Environmental Variable

---
+++ rules.py
@@ -0,0 +1,15 @@
+rules = [
+    "**Rule 1:**\nDo not hurt anyone by using racial, sexist, homophobic, degrading, discriminatory or any other multitude of slurs.",
+    "**Rule 2:**\nNo offensive nicknames, usernames, or statuses.",
+    "**Rule 3:**\nBe respectful to yourself and others. No Drama, Insults, or Hurting others.",
+    "**Rule 4:**\nSending explicit items is not tolerated. Anything relating to NSFW content should not appear on this server, however, some jokes are okay.",
+    "**Rule 5:**\nDo not reveal personal info of your own or anyone else's on this server.",
+    "**Rule 6:**\nDo not spam.",
+    "**Rule 7:**\nPINGING RULES: No pinging without reason. This includes pinging everyone. Abusing the teacher roles system to ping everyone will grant you an automatic mute. You can only ping classes if you have assigned teacher roles. Ghost pinging is not tolerated either.",
+    "**Rule 8:**\nDo not spread and/or talk about rumors.",
+    "**Rule 9:**\nDo not mention sensitive topics. This includes stopping a conversation about a certain topic when another member asks you to. If you or someone really needs help, please go to our counselors or call the suicide hotline. ",
+    "**Rule 10:**\nUpon joining this server, you have agreed that you are in ACP 8th or 7th grade. If you are not, admins will not hesitate to ban/kick you.",
+    "**Rule 11:**\nDo not talk in #advertisement-board.",
+    "**Rule 12:**\nSwearing is only tolerated as long as it's not extremely excessive and not used to insult another member. Any variations of the N word is not allowed.",
+    "**Rule 13:**\nAn infraction of these rules can lead to a reminder, official warning, kick, or ban. Admins and moderators will act accordingly to your behavior. If you believe we have done something wrongly, you can ask and explain to defend yourself, however, if you complain about it we will ignore your request completely. Bans can be appealed by DMing @Grub. "
+]
