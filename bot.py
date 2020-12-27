e6eea6e8ea0e080c5765eae32fe8a4ee32ee3c91
Rishil Kadakia <rishil.kadakia@gmail.com>
2020-12-27 13:15:56 -0700

    Added !mute, !unmute, !warn, and !ping.

--- bot.py
+++ bot.py
@@ -31,14 +31,14 @@ async def purge(ctx, amount):
 @client.command()
 @commands.has_permissions(kick_members = True)
 async def kick(ctx, member : discord.Member,*, reason = 'No Reason Provided'):
-    await ctx.send(f'{member} has been kicked from ACP Main for {reason}.')
+    await ctx.send(f'**{member}** has been kicked from ACP Main for {reason}.')
     await member.kick(reason=reason)
 
 # !ban <user>
 @client.command()
 @commands.has_permissions(ban_members = True)
 async def ban(ctx, member : discord.Member,*, reason = 'No Reason Provided'):
-    await ctx.send(f'{member} has been banned from ACP Main for {reason}.')
+    await ctx.send(f'**{member}** has been banned from ACP Main for {reason}.')
     await member.ban(reason=reason)
 
 # !unban <user>
@@ -51,8 +51,37 @@ async def unban(ctx, *, member):
         user = banned_entry.user
         if(user.name, user.discriminator) == (member_name, member_disc):
             await ctx.guild.unban(user)
-            await ctx.send(f'{member_name} has been unbanned.')
+            await ctx.send(f'**{member_name}** has been unbanned.')
             return
+
+# !mute <user>
+@client.command()
+@commands.has_permissions(kick_members=True)
+async def mute(ctx, member : discord.Member):
+    muted_role = ctx.guild.get_role(607034271990939658)
+    await member.add_roles(muted_role)
+    await ctx.send(f'**{member.mention}** has been muted.')
+
+# !unmute <user>
+@client.command()
+@commands.has_permissions(kick_members=True)
+async def unmute(ctx, member : discord.Member):
+    muted_role = ctx.guild.get_role(607034271990939658)
+    await member.remove_roles(muted_role)
+    await ctx.send(f'**{member.mention}** has been unmuted.')
+
+# !warn <user> <reason>
+@client.command()
+@commands.has_permissions(kick_members=True)
+async def warn(ctx, member : discord.Member, *, reason = 'No Reason Provided'):
+    await ctx.send(f'**{member}** has been warned for {reason}')
+
+# !ping
+@client.command()
+async def ping(ctx):
+    latency = str(client.latency * 1000)
+    decimal = latency.split('.')
+    await ctx.send(f'🏓 Pong! Latency: **{decimal[0]} ms**!')
 
 # !calc <number> <operator> <number>
 @client.command(aliases=['calculate'])
@@ -73,3 +102,4 @@ async def calc(ctx, *, equation):
             await ctx.send(f'Could not understand; incorrect format. Include a space between number and operator. Please make sure to perform !calc like this: <number> <operator> <number>.\nEx:\n- !calc 4 x 5\n- !calc 3345 + 123\n- !calc 54 / 3')
     except:
         await ctx.send(f'Could not understand; incorrect format. Include a space between number and operator. Please make sure to perform !calc like this: <number> <operator> <number>.\nEx:\n- !calc 4 x 5\n- !calc 3345 + 123\n- !calc 54 / 3')
+
